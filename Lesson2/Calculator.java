1493404e1345cf3945db837ea398114bef49a0fe
Mailtime <mailtime2010@gmail.com>
2018-05-31 23:28:41 +0300

    Task 4. to created Calculator.java

---
+++ Lesson2/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator{
+
+	public Calculator(){}
+	
+	public static void Caption(){
+		System.out.println("******************");
+		System.out.println("Calculator");
+		System.out.println("******************");
+	}
+	public static void EnterFirstNumber(){
+		System.out.println();
+		System.out.print("Enter first number: ");
+	}
+	public static void EnterSignMath(){
+		System.out.print("Enter the sign math. operation: ");
+	}
+	public static void EnterSecondNumber(){
+		System.out.print("Enter second number: ");
+	}
+	public static void EnterDoYouContinue(){
+			System.out.print("Do1 You want continue? [yes/no]: ");
+	}
+}
+
+
+
+
