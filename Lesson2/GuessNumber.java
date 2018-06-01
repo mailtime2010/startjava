47f87382651a983cf0dde2307724dc24e803ab73
Mailtime <mailtime2010@gmail.com>
2018-06-01 22:26:45 +0300

    .

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -9,10 +9,6 @@ public class GuessNumber{
 		return x; 
 	}
 		public static void EnterDoYouContinue(){
-			System.out.print("Do1 You want continue? [yes/no]: ");
+			System.out.print("Do You want continue? [yes/no]: ");
 	}
 }
-	//	if (x < a) { System.out.print("The number you entered is greater than what the computer.");} 
-	//	if (x > a) { System.out.print("The number you entered is less than what the computer.");} 
-	//	if (x == a) { System.out.print("You guessed!");}
-
