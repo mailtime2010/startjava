bbe5506a05998c576d3293ea84587baf80458ff1
Mailtime <mailtime2010@gmail.com>
2018-06-06 00:05:25 +0300

    тест редактора

--- src/com/startjava/lesson1/app/FirstApp.java
+++ src/com/startjava/lesson1/app/FirstApp.java
@@ -2,6 +2,6 @@ package com.startjava.lesson1.app;
 
 public class FirstApp{
 	public static void main(String[] args){
-	    System.out.println("Hello, world!");
+	    System.out.println("Hello, World!");
 	}
 }
