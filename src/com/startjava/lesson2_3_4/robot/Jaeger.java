941f1541c2e466860b1aae537801b597e17ddb6d
Mailtime <mailtime2010@gmail.com>
2018-06-07 00:43:12 +0300

    to Changed Lesson

--- src/com/startjava/lesson2_3_4/robot/Jaeger.java
+++ src/com/startjava/lesson2_3_4/robot/Jaeger.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson2.robot;
+package com.startjava.lesson2_3_4.robot;
 
 public class Jaeger{
 
--- src/com/startjava/lesson2_3_4/robot/JaegerTest.java
+++ src/com/startjava/lesson2_3_4/robot/JaegerTest.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson2.robot;
+package com.startjava.lesson2_3_4.robot;
 
 public class JaegerTest{
 	public static void main(String[] args){
